a9adc240ac6f9a059f220bf46a9ca2214308732d
Sarthak Vaish <srthk1993@gmail.com>
2016-07-19 19:14:08 -0400

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,8 +57,8 @@ $(function(){
             Backstretch Background Slider
     =========================================================================*/
     $("#welcome").backstretch([
-        "http://placehold.it/1200x768",
-        "http://placehold.it/1200x768",
+        "assets/images/GTMS_Team.jpg",
+        "assets/images/GTMS_Team.jpg",
         // "assets/images/your_pic.jpg" 
     ], {duration: 5000, fade: 400});
 
